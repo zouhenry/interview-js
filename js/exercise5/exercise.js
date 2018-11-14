04b4b449e3dab8b6b17dd5958a0b77f1332778a0
Henry Zou <HENRY.ZOU@netapp.com>
2018-11-14 08:41:40 -0500

    adding jsbin links to exercise 2,3,4,5

--- js/exercise5/exercise.js
+++ js/exercise5/exercise.js
@@ -1,11 +1,23 @@
-/**
- * Created by hzou on 4/21/16.
- */
 
-function exercise(x) {
-  return function (y) {
-    console.log(x, y);
-  };
+/*
+  1. create a helloMr function which takes a name to output "Hello Mr. <name>"
+  2. create a helloMrs function which takes a name to output "Hello Mrs. <name>"
+  3. create a helloMrAndMrs function which takes a name to output "Hello Mr. and Mrs. <name>"
+  4. create a generic partial function which takes a function and then partial fills in the parameters.
+  When it is invoked again with additional parameter, that's when the original function is resolved.
+  5. update helloMr, helloMrs, helloMrAndMrs, to use the function from #4
+*/
+
+function sayHello(title, name){
+    return 'Hello ' + title + ' ' + name;
 }
 
-//Given the function above, how would you invoke it to output "hello world"?
+//TODO: implement helloMr, helloMrs, helloMrAndMrs
+
+function exercise(){
+    console.log(helloMr('Smith')); //Hello Mr. Smith
+    console.log(helloMrs('Smith')); //Hello Mrs. Smith
+    console.log(helloMrAndMrs('Smith')); //Hello Mr. And Mrs. Smith
+}
+
+// exercise();
