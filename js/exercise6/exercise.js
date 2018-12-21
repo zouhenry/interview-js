ae543109ecb385e8bf775b6a1276511ba94c7350
Henry Zou <zouhenry@gmail.com>
2018-12-21 11:52:59 -0500

    updaing exercise 2-6

--- js/exercise6/exercise.js
+++ js/exercise6/exercise.js
@@ -1,15 +1,23 @@
-/**
- * Created by hzou on 4/21/16.
- */
 
-function exercise() {
-  var total = sum(2, 3);
-  console.log('total', total);
+/*
+  1. create a helloMr function which takes a name to output "Hello Mr. <name>"
+  2. create a helloMrs function which takes a name to output "Hello Mrs. <name>"
+  3. create a helloMrAndMrs function which takes a name to output "Hello Mr. and Mrs. <name>"
+  4. create a generic partial function which takes a function and then partial fills in the parameters.
+  When it is invoked again with additional parameter, that's when the original function is resolved.
+  5. update helloMr, helloMrs, helloMrAndMrs, to use the function from #4
+*/
 
-  var sum = function (param1, param2) {
-    return param1 + param2;
-  };
+function sayHello(title, name){
+    return 'Hello ' + title + ' ' + name;
 }
 
-//What happens when exercise function is called?
+//TODO: implement helloMr, helloMrs, helloMrAndMrs
+
+function exercise(){
+    console.log(helloMr('Smith')); //Hello Mr. Smith
+    console.log(helloMrs('Smith')); //Hello Mrs. Smith
+    console.log(helloMrAndMrs('Smith')); //Hello Mr. And Mrs. Smith
+}
+
 // exercise();
